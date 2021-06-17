c5f397ecf001bce6ee5fac19728ecbadfb3c88b1
Jean-Marc Epis <jeanmarc.epis@gmail.com>
2021-06-17 22:19:20 +0200

    First init

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Discord-WildBot
 Discord Bot for Wild Code School.
+
+Run npm i to install the module.
+
+You can then set the variables to be whatever you need and voila.
+Just run npm start to start the bot or npm run dev.
+
+Just change the Token in config.json to use your own bot with the same commands.
---
+++ index.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+
+// Lien d'invitation : https://discordapp.com/api/oauth2/authorize?client_id=855129296166584342&permissions=8&scope=bot
+
+client.login(config.token);
+
+client.once("ready", () => {
+    console.log("Le WildBot est en route !")
+    client.user.setActivity("Surveille les Wilders", {type: "STREAMING", url: "https://www.twitch.tv/lonelycrow_"});
+
+    const id = "855136058277363743";
+    const messageChannel = client.channels.cache.find(channel => channel.id === id);
+    const messages = [
+        "Hello",
+        "How",
+        "Are",
+        "You",
+        "?"
+    ]
+    let i = 0;
+
+    const sendMessagesAtInterval = () => {
+        messageChannel.send(messages[i]);
+        i++;
+    }
+    const interval = setInterval(() => {
+        sendMessagesAtInterval()
+        if(i >= messages.length) {
+            clearInterval(interval)
+            console.log("Il n'y a plus aucun message à envoyer");
+            i = 0;
+        }
+    }, 5000);
+});
