f033b8759e4a4a80b8c7de468195838f3347a37c
Jean-Marc Epis <jeanmarc.epis@gmail.com>
2021-06-17 22:25:30 +0200

    Made the code more user friendly

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 
-// Lien d'invitation : https://discordapp.com/api/oauth2/authorize?client_id=855129296166584342&permissions=8&scope=bot
+// invitation link : https://discordapp.com/api/oauth2/authorize?client_id=855129296166584342&permissions=8&scope=bot
 
 client.login(config.token);
 
@@ -10,7 +10,7 @@ client.once("ready", () => {
     console.log("Le WildBot est en route !")
     client.user.setActivity("Surveille les Wilders", {type: "STREAMING", url: "https://www.twitch.tv/lonelycrow_"});
 
-    const id = "855136058277363743";
+    const id = "855136058277363743"; // Put the channel id here
     const messageChannel = client.channels.cache.find(channel => channel.id === id);
     const messages = [
         "Hello",
